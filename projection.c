d07ae5c7d3e337251c3458addb1831dab7d004d6
Alexis Praga <alexis.praga@free.fr>
2013-03-29 21:30:25 +0100

    Now we can fill the grid as each cell is a closed path. Use the fill_cell variable for that

--- projection.c
+++ projection.c
@@ -3,14 +3,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
+#include <stdlib.h>
+
 
 #define PI 3.141592653589793238462643383
 #define ratio PI/180.
+#define POINT_UNDEFINED -99999.
 
 /* Modify these */
 #define lat_0 10*ratio
 #define lon_0 0*ratio
-#define nb_lon 20
+#define nb_lon 10
 #define nb_lat 10
 
 #define radius 10
@@ -18,8 +22,10 @@
 #define height 500
 #define x_center width/2
 #define y_center height/2
-#define nb_points 30
+#define nb_points 20
 #define scale 20.
+
+#define fill_cell 0
 
 /* Input in degrees */
 int map_to_orthographic(float* x, float* y, float lat, float lon)
@@ -31,7 +37,8 @@ int map_to_orthographic(float* x, float* y, float lat, float lon)
   dist = sin(lat_0)*sin(lat1) + cos(lat_0)*cos(lat1)*cos(lon1 - lon_0);
 
   /* Only print negative distance, with a precision */
-  if (dist > prec) return -1;
+  /* No longer need to check that */
+//  if (dist < prec) return -1;
 
   (*x) = radius*cos(lat1)*sin(lon1 - lon_0);
   (*y) = cos(lat_0)*sin(lat1) - sin(lat_0)*cos(lat1)*cos(lon1 - lon_0);
@@ -40,6 +47,7 @@ int map_to_orthographic(float* x, float* y, float lat, float lon)
   return 1;
 }
 
+/* Draw the visible part of an arc */
 void draw_arc(float lat0, float lon0, float lat1, float lon1, cairo_t* cr) 
 {
   int i;
@@ -51,31 +59,101 @@ void draw_arc(float lat0, float lon0, float lat1, float lon1, cairo_t* cr)
   dlat = (lat1 - lat0) / nb_points;
   dlon = (lon1 - lon0) / nb_points;
 //  printf("dlat dlon %f %f \n", dlat, dlon);
-
   for (i = 0; i < nb_points; ++i)
   {
     res1 = map_to_orthographic(&x1, &y1, lat0 + i*dlat, lon0 + i*dlon);
     res2 = map_to_orthographic(&x2, &y2, lat0 + (i+1)*dlat, lon0 + (i+1)*dlon);
 
     if (res1 > 0 && res2 > 0) {
-      cairo_move_to(cr, x_center + x1*scale, y_center + y1*scale);
-      cairo_line_to(cr, x_center + x2*scale, y_center + y2*scale);
-      cairo_stroke(cr);
+      /* Beware, y=0 is top of the figure, so we must revert */
+      if (fill_cell) {
+        cairo_line_to(cr, x_center + x2*scale, height-(y_center + y2*scale));
+        cairo_stroke_preserve(cr);
+      }
+      else {
+        cairo_move_to(cr, x_center + x1*scale, height-(y_center + y1*scale));
+        cairo_line_to(cr, x_center + x2*scale, height-(y_center + y2*scale));
+        cairo_stroke(cr);
+      }
+
     }
   }
 }
 
-/* (lat, lon) is upper left corner */
+void visible_lon_extrema(float* maxlon1, float* maxlon2, float lat)
+{
+  /* Lat is in degree */
+  *maxlon1 = -sin(lat_0)*sin(lat*ratio)/(cos(lat_0)*cos(lat*ratio));
+  *maxlon1 = (acos(*maxlon1) + lon_0)*180./PI;
+  *maxlon2 = 360 - *maxlon1;
+}
+
+void adapt_lon(float* first_lon, float* last_lon, float lat) {
+  float max_lon1, max_lon2;
+  int x;
+
+  
+  visible_lon_extrema(&max_lon1, &max_lon2, lat);
+  if (*first_lon < max_lon1 && *last_lon < max_lon1) 
+    x=0;
+  else if (*first_lon < max_lon1 && *last_lon > max_lon1) 
+    *last_lon = max_lon1;
+  else if (*first_lon < max_lon2 && *last_lon < max_lon2)
+  {
+    *first_lon = POINT_UNDEFINED;
+    *last_lon = POINT_UNDEFINED;
+    return;
+  }
+  else if (*first_lon < max_lon2 && *last_lon > max_lon2)
+    *first_lon = max_lon2;
+}
+
+/* (lat, lon) is upper left corner and in degree */
 /* dlat > 0 */
 void draw_cell(float lat, float lon, float dlat, float dlon, cairo_t *cr){
   float x1, y1;
   float x2, y2;
+  float first_lon, last_lon;
+  float first_lon2, last_lon2;
+  float r, g, b;
 
-  draw_arc(lat, lon, lat, lon + dlon, cr);
-  draw_arc(lat, lon + dlon, lat - dlat, lon + dlon, cr);
-  draw_arc(lat - dlat, lon + dlon, lat - dlat, lon, cr);
-  draw_arc(lat - dlat, lon, lat, lon, cr);
-  
+  first_lon = lon;
+  last_lon = lon+dlon;
+  adapt_lon(&first_lon, &last_lon, lat);
+
+  first_lon2 = lon;
+  last_lon2 = lon+dlon;
+  adapt_lon(&first_lon2, &last_lon2, lat - dlat);
+
+//  if (first_lon != POINT_UNDEFINED && last_lon != POINT_UNDEFINED) 
+//    draw_arc(lat, first_lon, lat, last_lon, cr);
+//
+//  if (last_lon != POINT_UNDEFINED && last_lon2 != POINT_UNDEFINED) 
+//    draw_arc(lat, last_lon, lat - dlat, last_lon2, cr);
+//
+//  if (last_lon2 != POINT_UNDEFINED && first_lon2 != POINT_UNDEFINED) 
+//    draw_arc(lat - dlat, last_lon2, lat - dlat, first_lon2, cr);
+//
+//  if (first_lon2 != POINT_UNDEFINED && first_lon != POINT_UNDEFINED) 
+//    draw_arc(lat - dlat, first_lon2, lat, first_lon, cr);
+
+  if (first_lon2 != POINT_UNDEFINED && first_lon != POINT_UNDEFINED && 
+      last_lon2 != POINT_UNDEFINED && last_lon != POINT_UNDEFINED ) 
+  {
+    cairo_set_source_rgb(cr, 0., 0., 0.);
+    draw_arc(lat, first_lon, lat, last_lon, cr);
+    draw_arc(lat, last_lon, lat - dlat, last_lon2, cr);
+    draw_arc(lat - dlat, last_lon2, lat - dlat, first_lon2, cr);
+    draw_arc(lat - dlat, first_lon2, lat, first_lon, cr);
+    if (fill_cell) {
+      cairo_close_path(cr);
+      r = (float)rand()/(float)RAND_MAX;
+      g = (float)rand()/(float)RAND_MAX;
+      b = (float)rand()/(float)RAND_MAX;
+      cairo_set_source_rgb(cr, r, g, b);
+      cairo_fill(cr);
+    }
+  }
 }
 
 void draw_outer_circle(cairo_t *cr)
@@ -90,12 +168,14 @@ int main (int argc, char *argv[])
   FILE *input;
   cairo_surface_t *cs;
   cairo_t *cr;
-/*  int height = 100;
-  int width = 100;*/
+  /*  int height = 100;
+      int width = 100;*/
   float margin = 5.;
   float dlon, dlat;
   float lon;
   int i, j;
+
+  srand(time(NULL));
 
   /* Create a surface and a cairo_t */
   /* A4 format approx */
@@ -108,14 +188,14 @@ int main (int argc, char *argv[])
   dlon = 360./nb_lon;
   dlat = 180./nb_lat;
   for (i = 0; i < nb_lat; ++i) {
-  //for (i = nb_lat/2; i < nb_lat/2+1; ++i) {
-//    printf("lat latnext %f %f \n", 90.-i*dlat, dlat);
+  //for (i = 3; i < 4; ++i) {
+    //for (j = 0; j < 1; ++j) {
     for (j = 0; j < nb_lon; ++j) {
       draw_cell(90.-i*dlat, j*dlon, dlat, dlon, cr);
     }
   }
 
-  draw_outer_circle(cr);
+  //draw_outer_circle(cr);
 
   /* Needed for PDF output */
   cairo_show_page(cr);
